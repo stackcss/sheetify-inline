1bbb8c4af528e09d12ca72fa00fdd46f8338a0b4
zhouhansen <z308114274@gmail.com>
2017-10-11 22:40:28 +0800

    use css-reurl

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var base64Img = require('base64-img')
-var cssurl = require('css-url-rewriter')
+var cssurl = require('css-reurl')
 
 module.exports = transform
 
 function transform (filename, source, options, done){
   try {
-    source = cssurl(source, url=>{
+    cssurl(source, url=>{
       return base64Img.base64Sync(url)
+    }, source=>{
+      done(null, source)
     })
   } catch (e) {
     return done(e)
   }
-
-  done(null, source)
 }
