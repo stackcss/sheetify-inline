f92f26d07cc359e95c098e94cc69e7103517cf74
zhouhansen <z308114274@gmail.com>
2017-10-11 16:30:44 +0800

    1.0.0

---
+++ test.js
@@ -0,0 +1,24 @@
+const transform = require('./')
+const test = require('tape')
+const path = require('path')
+const fs = require('fs')
+
+test('basic', compare('basic.css', 'basic-out.css'))
+
+function compare (inputFile, outputFile) {
+  return function compareTest (t) {
+    const file = path.join(__dirname, 'fixtures', inputFile)
+    const src = fs.readFileSync(file, 'utf8')
+    const expected = fs.readFileSync(
+      path.join(__dirname, 'fixtures', outputFile)
+    , 'utf8')
+
+    t.plan(1)
+
+    transform(file, src, {}, function(err, actual){
+      if (err) return t.error(err)
+
+      t.equal(actual, expected, 'output is as expected')
+    })
+  }
+}
